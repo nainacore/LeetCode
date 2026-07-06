8cf419be97e551b3133b84a52b10e8e6590ebf0c
Sunaina Mohapatra <151989225+nainacore@users.noreply.github.com>
2026-07-07 00:16:21 +0530

    Time: 2 ms (70.79%), Space: 47.4 MB (33.55%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int min=nums[0];
+        int max=nums[0];
+        int res=nums[0];
+        for(int i=1;i<nums.length;i++){
+           if(nums[i]<0){
+             int temp=max;
+             max=min;
+             min=temp;
+           }
+           max=Math.max(nums[i],max*nums[i]);
+           min=Math.min(nums[i],min*nums[i]);
+           res=Math.max(max,res);
+
+        }
+        return res;
+    }
+}
